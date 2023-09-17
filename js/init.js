b624fe3479d9c7c81666a4cd6a14e5adf7a67c95
Manikandan Raju <67756241+Manikandan-Raju@users.noreply.github.com>
2023-09-17 08:22:36 +0000

    email php remove

--- js/init.js
+++ js/init.js
@@ -162,30 +162,6 @@
           alert("mail sent successfully")
         });
 
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
       return false;
    });
 
